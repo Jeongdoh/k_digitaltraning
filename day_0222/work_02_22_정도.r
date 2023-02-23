e28daaebad61f5ae078698ced0cc1ecfb67b9396
Jeongdoh <y2kjd@naver.com>
2023-02-23 17:07:25 +0900

    asdf

--- day_0222/work_02_22_정도.r
+++ day_0222/work_02_22_정도.r
@@ -1,40 +1,3 @@
-#-----------------------------------------------------------
-# 공통으로 자주 사용되는 함수들
-#-----------------------------------------------------------
-
-# 객체의 기본 정보 출력 기능 함수----------------
-displayInfo<-function(data){
-    print("TEST")
-    str(data)
-    summary(data)
-}
-
-
-# 결측치 체크 기능 함수------------------------------
-checkNa<-function(data){
-    eNum<-sum(is.na(data))
-    rNum<-sum(!complete.cases(data))
-    print(paste("원소 Na :",eNum,"행 NA",rNum,sep = " - "))
-}
-
-library(stringr)
-
-
-# 최빈값 체크 기능 함수----------------------------------------
-checkMode<-function(data){
-    for(col in colnames(data))
-    {
-        # 컬럼별 빈도표 >> 최대값
-        max(table(data[col]))           # 가장 많은 숫자
-        maxIdx<-which.max(table(data[col]))     # 인덱스
-        maxValue<-names(which.max(table(data[col])))     # 가장 많이 나온 값
-
-        print(paste(col,"최빈값 : ", maxValue))
-    }
-}
-
-
-
 # 이상치 체크 기능 함수-------------------------------------
 # 과제 => 여러개
 
@@ -110,16 +73,3 @@ find_outliers_dbscan(data)
 
 # DBSCAN은 데이터의 밀도가 높은 지역을 클러스터로 인식하고, 그렇지 않은 지점들은 이상치로 판단. 
 # DBSCAN 방법은 클러스터링 알고리즘으로, 비정상적으로 밀도가 낮은 데이터들을 노이즈로 판단.
-
-
-
-
-
-
-
-
-# 전체 데이터의 상관계수 체크기능 함수------------------------
-# 매개변수 => 데이터 객체, 기준 컬럼명
-checkCor<-function(data){
-
-}
